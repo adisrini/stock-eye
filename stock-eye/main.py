fcf704891ea20dbdfdb0b9bd72b502b5c1072fe2
Aditya Srinivasan <asrinivasan@cinchapi.com>
2018-01-11 12:19:21 +0530

    Create FB chat trigger upon buy signal

--- stock-eye/main.py
+++ stock-eye/main.py
@@ -1,43 +1,72 @@
 from pandas_datareader import data as pdr
 from datetime import datetime, timedelta
+from fbchat import Client
+from fbchat.models import *
 import fix_yahoo_finance as yf
 import time
+import yaml
+import warnings
+
+warnings.simplefilter("ignore", ResourceWarning)
+warnings.simplefilter("ignore", DeprecationWarning)
 
 yf.pdr_override()
 
-ticker = "MU"
+tickers = ["MU", "INTC", "AMD"]
 period = 100
 comp_factor = 2
+secrets = yaml.load(open('secrets.yaml'))
+prev_closing_price = {}
+prev_moving_average = {}
+curr_closing_price = {}
+curr_moving_average = {}
+
+for ticker in tickers:
+    prev_closing_price[ticker] = -1
+    prev_moving_average[ticker] = -1
 
 while(True):
     end = datetime.today()
     start = end - timedelta(days=comp_factor*period)
 
-    df = pdr.get_data_yahoo(ticker, start=start, end=end)
+    for ticker in tickers:
+        df = pdr.get_data_yahoo(ticker, start=start, end=end)
 
-    # cum_sums = {}
-    #
-    # for i in range(comp_factor*period, -1, -1):
-    #     dt = end - timedelta(days=i)
-    #     dt_str = dt.strftime('%Y-%m-%d')
-    #     try:
-    #         cum_sums[dt_str] = df.loc[dt_str].Close
-    #     except KeyError as e:
-    #         pass
+        tot = 0
+        num = 0
+        for i in range(0, period):
+            dt = end - timedelta(days=i)
+            dt_str = dt.strftime('%Y-%m-%d')
+            try:
+                tot += df.loc[dt_str].Close
+                num += 1
+            except KeyError as e:
+                pass
 
-    tot = 0
-    num = 0
-    for i in range(0, period):
-        dt = end - timedelta(days=i)
-        dt_str = dt.strftime('%Y-%m-%d')
-        try:
-            tot += df.loc[dt_str].Close
-            num += 1
-        except KeyError as e:
-            pass
+        curr_closing_price[ticker] = df['Close'][len(df) - 1]
+        curr_moving_average[ticker] = tot/num
 
-    print("\n")
-    if(num != 0):
-        print("Moving average:            {:f}".format(tot/num))
-        print("Current closing price:     {:f}".format(df.loc[end.strftime('%Y-%m-%d')].Close))
-    time.sleep(5)
+        signal_buy = False
+
+        if prev_moving_average[ticker] == -1:
+            prev_moving_average[ticker] = curr_moving_average[ticker]
+
+        if prev_closing_price[ticker] == -1:
+            prev_closing_price[ticker] = curr_closing_price[ticker]
+
+        if prev_closing_price[ticker] < prev_moving_average[ticker] and curr_closing_price[ticker] > curr_moving_average[ticker]:
+            signal_buy = True
+
+        print("\n")
+        if(num != 0):
+            print(" ==== {} ==== ".format(ticker))
+            print("Moving average:            {:f}".format(tot/num))
+            print("Current closing price:     {:f}".format(curr_closing_price[ticker]))
+            print("Signal buy:                {:b}".format(signal_buy))
+
+        if signal_buy:
+            client = Client(secrets['email'], secrets['password'])
+            client.send(Message(text="Buy {} at {}".format(ticker, curr_closing_price[ticker])), thread_id=client.uid, thread_type=ThreadType.USER)
+            client.logout()
+
+    time.sleep(600)
